1d09a841bb188dd996d002d144077934b6ac4cca
maheshsabnis <sabnis_m@hotmail.com>
2023-06-20 08:57:56 +0530

    Day2

--- CS_SimpleClass/Program.cs
+++ CS_SimpleClass/Program.cs
@@ -2,6 +2,7 @@
 
 
 
+using CS_SimpleClass;
 using CS_SimpleClass.AppClasses;
 
 Console.WriteLine("Demo Simple Operation class");
@@ -32,6 +33,14 @@ Console.WriteLine($"{a} / {b} is = {obj.Division(a,b)}");
 string Name = string.Empty;
 Console.WriteLine($"Reverse of {Name} is = {obj.Reverse(Name)}");
 
+string str = $"The James Bond series focuses on James Bond, a fictional British Secret Service agent created in 1953 by writer Ian Fleming, who featured him in twelve novels and two short-story collections. Since Fleming's death in 1964, eight other authors have written authorised Bond novels or novelisations: Kingsley Amis, Christopher Wood, John Gardner, Raymond Benson, Sebastian Faulks, Jeffery Deaver, William Boyd, and Anthony Horowitz. The latest novel is With a Mind to Kill by Anthony Horowitz, published in May 2022. Additionally Charlie Higson wrote a series on a young James Bond, and Kate Westbrook wrote three novels based on the diaries of a recurring series character, Moneypenny.\r\n\r\nThe character—also known by the code number 007 (pronounced \"double-oh-seven\")—has also been adapted for television, radio, comic strip, video games and film. The films are one of the longest continually running film series and have grossed over US$7.04 billion in total at the box office, making it the fifth-highest-grossing film series to date, which started in 1962 with Dr. No, starring Sean Connery as Bond. As of 2021, there have been twenty-five films in the Eon Productions series. The most recent Bond film, No Time to Die (2021), stars Daniel Craig in his fifth portrayal of Bond; he is the sixth actor to play Bond in the Eon series. There have also been two independent productions of Bond films: Casino Royale (a 1967 spoof starring David Niven) and Never Say Never Again (a 1983 remake of an earlier Eon-produced film, 1965's Thunderball, both starring Connery). In 2015, the series was estimated to be worth $19.9 billion in total (based on box-office grosses, DVD sales and merchandise tie-ins),[1] making James Bond one of the highest-grossing media franchises of all time.";
+
+
+StringOperations stringOperations = new StringOperations();
+
+Console.WriteLine($"Count of statements {stringOperations.GetStatementCount(str)}");
+
+Console.WriteLine($"Count of words {stringOperations.GetWordCount(str)}");
 
 
 Console.ReadLine();
---
+++ CS_SimpleClass/StringOperations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_SimpleClass
+{
+    internal class StringOperations
+    {
+        public int GetStatementCount(string code)
+        {
+             
+            string[] statements = code.Split('.');
+
+           
+            for (int i = 0; i < statements.Length; i++)
+            {
+                statements[i] = statements[i].Trim();
+            }
+
+            int statementCount = 0;
+            foreach (string statement in statements) 
+            {
+                if (!IsSalutation(statement))
+                {
+                    statementCount++;
+                }
+            }
+             
+          
+
+            return statementCount;
+        }
+
+
+        public int GetWordCount(string text)
+        {
+            // Split the string into an array of words based on whitespace characters
+            string[] words = text.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Return the number of words
+            return words.Length;
+        }
+
+        private bool IsSalutation(string statement)
+        {
+            string[] s = { "Dr",  "Mr", "Mrs", "Mast", "Miss" };
+            string firstWord = statement.Split(' ').FirstOrDefault();
+            return s.Contains(firstWord);
+        }
+    }
+}
